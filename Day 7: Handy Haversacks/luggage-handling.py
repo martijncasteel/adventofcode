625c95c10742648f13043d67c9083263413afcc1
Martijn Casteel <martijncasteel@users.noreply.github.com>
2020-12-26 14:29:43 +0100

    A lot of bags, used test for veryfiying with the written example

---
+++ Day 7: Handy Haversacks/luggage-handling.py
@@ -0,0 +1,43 @@
+# key sits inside values
+bags = {}
+
+# light red bags contain 1 bright white bag, 2 muted yellow bags.
+def process(line):
+  bag, contains = line.split(" contain ", 2)
+
+  # remove bags from name
+  bag = ' '.join(bag.split(' ')[:-1])
+
+  if contains == "no other bags.":
+    return
+  else:
+
+    # retrieve color from every bag
+    for b in contains.split(", "):
+      color = ' '.join(b.split(' ')[1:3])
+      bags.setdefault(color,[]).append(bag)
+
+
+with open('input', 'r') as file:
+  for line in file:
+    process(line.strip())
+  process(line.strip())
+
+
+# use bags to find possible bags to find a shiny gold bag
+def find(color):
+  yield color
+
+  if color in bags:
+    for bag in bags[color]:
+
+      # for every bag it can contain yield it
+      for b in find(bag):
+        yield b
+
+
+options = set(find("shiny gold"))
+options.remove("shiny gold")
+print(f'part1 -> {len(options)}')
+
+
---
+++ Day 7: Handy Haversacks/luggage-sum-contents.py
@@ -0,0 +1,46 @@
+# key has the following bags
+bags = {}
+
+# light red bags contain 1 bright white bag, 2 muted yellow bags.
+def process(line):
+  if line == "":
+    return
+
+  bag, contains = line.split(" contain ", 2)
+
+  # remove bags from name
+  bag = ' '.join(bag.split(' ')[:-1])
+
+  if contains == "no other bags.":
+    return
+  else:
+
+    # retrieve color from every bag
+    for b in contains.split(", "):
+      count, color = b.split(' ')[:1], ' '.join(b.split(' ')[1:3])
+
+      # swapped these around
+      bags.setdefault(bag,{})
+      bags[bag][color] = int(count[0])
+
+
+with open('input', 'r') as file:
+  for line in file:
+    process(line.strip())
+  process(line.strip())
+
+
+# count the number of bags a gold bag has
+def count(color):
+  sum = 0
+
+  if color in bags:
+    for bag, number in bags[color].items():
+      sum += number + number * count(bag)
+
+  return sum
+
+
+print(f'part2 -> {count("shiny gold")}')
+
+
