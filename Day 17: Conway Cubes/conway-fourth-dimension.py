996fe2c5ab1b634069ca892c03bfc8227b96f255
Martijn Casteel <martijncasteel@users.noreply.github.com>
2020-12-27 13:30:58 +0100

    Fixed day 17, compared to default False instead of previous active..

--- Day 17: Conway Cubes/conway-fourth-dimension.py
+++ Day 17: Conway Cubes/conway-fourth-dimension.py
@@ -10,18 +10,19 @@ class Cube:
     sum = 0
 
     # another 3 dimensional loop
-    for z in range(self.vector[2] -1, self.vector[2] +2):
-      for y in range(self.vector[1] -1, self.vector[1] +2):
-        for x in range(self.vector[0] -1, self.vector[0] +2):
+    for w in range(self.vector[3] -1, self.vector[3] + 2):
+      for z in range(self.vector[2] -1, self.vector[2] + 2):
+        for y in range(self.vector[1] -1, self.vector[1] +2):
+          for x in range(self.vector[0] -1, self.vector[0] +2):
 
-          if (x,y,z) == self.vector:
-            continue
+            if (x,y,z,w) == self.vector:
+              continue
 
-          if (x,y,z) not in previous:
-            continue
-          
-          if previous[(x,y,z)].active:
-            sum += 1
+            if (x,y,z,w) not in previous:
+              continue
+            
+            if previous[(x,y,z,w)].active:
+              sum += 1
 
     if self.vector in previous:
       if previous[self.vector].active and sum >= 2 and sum <= 3:
@@ -44,6 +45,9 @@ class Cube:
 
   # some method to sort a single pocket for printing
   def __lt__(a, b):
+    if a.vector[3] != b.vector[3]:
+      return a.vector[3] < b.vector[3]
+
     if a.vector[2] != b.vector[2]:
       return a.vector[2] < b.vector[2]
 
@@ -57,6 +61,9 @@ class Cube:
     raise AttributeError()
 
   def __gt__(a, b):
+    if a.vector[3] != b.vector[3]:
+      return a.vector[3] > b.vector[3]
+
     if a.vector[2] != b.vector[2]:
       return a.vector[2] > b.vector[2]
 
@@ -77,11 +84,12 @@ def print_pockets(dict):
   for cube in sorted(dict.values()):
     
     if not previous:
-      print(f'z={cube.vector[2]}')
+      print(f'z={cube.vector[2]}, w={cube.vector[3]}')
       print(cube, end ='')
 
-    elif previous.vector[2] != cube.vector[2]:
-      print(f'\n\nz={cube.vector[2]}')
+    elif previous.vector[2] != cube.vector[2] or previous.vector[3] != cube.vector[3]:
+      print(f'\n\nz={cube.vector[2]}, w={cube.vector[3]}\n')
+      print(cube, end ='')
 
     elif previous.vector[1] != cube.vector[1]:
       print(f'\n{cube}', end='')
@@ -98,7 +106,7 @@ with open('input', 'r') as file:
     characters = list(line.strip())
 
     for x, char in enumerate(characters):
-      pockets[(x,y,0)] = Cube((x, y, 0), char == "#")
+      pockets[(x,y,0, 0)] = Cube((x, y, 0, 0), char == "#")
 
   cycles.append(pockets.copy())
   pockets = {}
@@ -114,17 +122,18 @@ for cycle in range(1, 7):
   height = max(cycles[-1].items())[0][1]
   width = max(cycles[-1].items())[0][0]
   
-  for z in range(-cycle, cycle + 1):
-    for y in range(-cycle, height + cycle + 1):
-      for x in range(-cycle, width + cycle + 1):
-        
-        #create new cube
-        cube = Cube((x,y,z))
-        cube.compute(cycles[-1])
-        pockets[(x,y,z)] = cube
+  for w in range(-cycle, cycle + 1):
+    for z in range(-cycle, cycle + 1):
+      for y in range(-cycle, height + cycle + 1):
+        for x in range(-cycle, width + cycle + 1):
+          
+          #create new cube
+          cube = Cube((x,y,z,w))
+          cube.compute(cycles[-1])
+          pockets[(x,y,z,w)] = cube
 
-        if cube.active:
-          sum += 1
+          if cube.active:
+            sum += 1
 
 
   cycles.append(pockets.copy())
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Advent of Code 2020
-I came across the advent of code on the 25th. I have done a lot of programming but not in Python and since it is the main language for the team I'm currently in. I thought why not give it a try.
+I came across the [advent of code](https://adventofcode.com/2020) on the 25th. I have done a lot of programming but not in Python and since it is the main language for the team I'm currently in. I thought why not give it a try.
