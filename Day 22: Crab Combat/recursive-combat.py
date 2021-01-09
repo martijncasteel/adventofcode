cfa1e07ffbe045388d523cf7e56668af2ab03c32
Martijn Casteel <martijncasteel@users.noreply.github.com>
2021-01-09 12:56:02 +0100

    Fixed a bug in recursive-combat.py, correct answer is now given!

--- Day 22: Crab Combat/recursive-combat.py
+++ Day 22: Crab Combat/recursive-combat.py
@@ -1,3 +1,12 @@
+import sys, logging
+
+# with input and printing it all it takes too long 
+# _debug_ for all, _info_ for the answer only
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger()
+
+max_game_number = 0
+
 class Player: 
   def __init__(self, data = []):
     self.cards = []
@@ -23,61 +32,65 @@ class Player:
   def draw(self):
     return self.cards.pop(0)
 
+
   def copy(self, n):
     p = Player()
     p.cards = [self.cards[i] for i in range(n)]
     return p
 
-
 def game(p1, p2, game_number = 1):
   history = set()
   round = 1
 
-  print(f'\n=== Game {game_number} ===')
+  logger.debug(f'\n=== Game {game_number} ===')
+
+  # so I probably shouldn't use global
+  global max_game_number
+  if game_number > max_game_number:
+    max_game_number = game_number 
 
   while True:
 
     if len(p1.cards) == 0:
-      print(f'The winner of game {game_number} is player 2!')
-      return p2
+      logger.debug(f'The winner of game {game_number} is player 2!')
+      return 2
 
     elif len(p2.cards) == 0:
-      print(f'The winner of game {game_number} is player 1!')
-      return p1
+      logger.debug(f'The winner of game {game_number} is player 1!')
+      return 1
 
-
-    print(f'\n-- Round {round} (Game {game_number}) --')
-    print(f'Player 1\'s deck: { ", ".join([str(c) for c in p1.cards]) }')
-    print(f'Player 2\'s deck: { ", ".join([str(c) for c in p2.cards]) }')
+    logger.debug(f'\n-- Round {round} (Game {game_number}) --')
+    logger.debug(f'Player 1\'s deck: { ", ".join([str(c) for c in p1.cards]) }')
+    logger.debug(f'Player 2\'s deck: { ", ".join([str(c) for c in p2.cards]) }')
 
     # if this game occurred before player 1 wins
-    if (p1.calculate(), p2.calculate()) in history:
-      print('-- history repeats itself, player 1 wins --')
-      return p1
-
-    history.add((p1.calculate(), p2.calculate()))
+    round_hash = (p1.calculate(), p2.calculate())
 
     # draw a card each, current play
     draw = (round, p1.draw(), p2.draw())
-    print(f'Player 1 plays: { draw[1] }')
-    print(f'Player 2 plays: { draw[2] }')
+    logger.debug(f'Player 1 plays: { draw[1] }')
+    logger.debug(f'Player 2 plays: { draw[2] }')
+
+
+    # check if history repeats itself
+    if round_hash in history:
+      logger.debug('-- history repeats itself, player 1 wins --')
+      return 1
+
+    history.add(round_hash)
 
     if len(p1.cards) >= draw[1] and len(p2.cards) >= draw[2]:
       # play another game as rules depict
-      print('Playing a sub-game to determine the winner...')
+      logger.debug('Playing a sub-game to determine the winner...')
 
-      winner = game(p1.copy(draw[1]), p2.copy(draw[2]), game_number + 1)
+      winner = game(p1.copy(draw[1]), p2.copy(draw[2]), max_game_number + 1)
+      winner = p1 if winner == 1 else p2
 
-      print(f'\n...anyway, back to game {game_number}.')
+      logger.debug(f'\n...anyway, back to game {game_number}.')
 
     else:
       # game like in previous part, simply highest card wins
-
-      if draw[1] > draw[2]:
-        winner = p1
-      else:
-        winner = p2
-
+      winner = p1 if draw[1] > draw[2] else p2
 
       
     # give the winner the cards
@@ -86,14 +99,14 @@ def game(p1, p2, game_number = 1):
       p1.cards.append(draw[1])
       p1.cards.append(draw[2])
 
-      print(f'Player 1 wins round {round} of game {game_number}!')
+      logger.debug(f'Player 1 wins round {round} of game {game_number}!')
 
     else:
       # player 2 won!
       p2.cards.append(draw[2])
       p2.cards.append(draw[1])
 
-      print(f'Player 2 wins round {round} of game {game_number}!')
+      logger.debug(f'Player 2 wins round {round} of game {game_number}!')
 
     round += 1
 
@@ -107,10 +120,11 @@ p1 = Player(data[0].split('\n'))
 p2 = Player(data[1].split('\n'))
 
 winner = game(p1, p2)
+winner = p1 if winner == 1 else p2
 
-print('\n== Post-game results ==')
-print(f'Player 1\'s deck: { ", ".join([str(c) for c in p1.cards]) }')
-print(f'Player 2\'s deck: { ", ".join([str(c) for c in p2.cards]) }')
-print(winner.calculate())
+logger.info('\n== Post-game results ==')
+logger.info(f'Player 1\'s deck: { ", ".join([str(c) for c in p1.cards]) }')
+logger.info(f'Player 2\'s deck: { ", ".join([str(c) for c in p2.cards]) }')
+logger.info(winner.calculate())
 
 
