eff0060bde0f6fc2f2447468ae3c3b034e7e3501
Martijn Casteel <martijncasteel@users.noreply.github.com>
2022-12-05 20:36:47 +0100

    2022: Day 1 calories

---
+++ 2022/Day 1: Calorie Counting/calorie-counter.py
@@ -0,0 +1,24 @@
+most_calories = 0
+current_calories = 0
+
+calories = []
+
+with open('input', 'r') as file:
+  for line in file:
+    if line.strip() == '':
+      # if current elf has more calores, store it
+      if current_calories > most_calories:
+        most_calories = current_calories
+
+      calories.append(current_calories)
+      current_calories = 0
+
+    else:
+      current_calories += int(line)
+
+print(f'part 1: {most_calories}')
+
+calories.sort(reverse=True)
+print(f'part 2: {sum(calories[:3])}')
+
+exit(0)
